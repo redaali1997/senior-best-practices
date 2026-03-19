d003e5c2f945f601f57cbb7fbb73bcda4b15e54a
Reda Ali <reda5201028@gmail.com>
2026-03-19 04:01:01 +0200

    Job tries and backoff

--- app/Jobs/SendWelcomeEmailJob.php
+++ app/Jobs/SendWelcomeEmailJob.php
@@ -6,12 +6,14 @@ use App\Models\User;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Support\Facades\Mail;
+use Throwable;
 
 class SendWelcomeEmailJob implements ShouldQueue
 {
     use Queueable;
 
-    protected $ret
+    public $tries = 3;
+    public $backoff = 120;
 
     /**
      * Create a new job instance.
@@ -27,5 +29,9 @@ class SendWelcomeEmailJob implements ShouldQueue
     public function handle(): void
     {
         // Sending email here
+    }
+
+    public function failed(Throwable $throwable) {
+        Log::error("Failed to send welcome email to user {$this->user->id}: {$throwable->getMessage()}");
     }
 }
