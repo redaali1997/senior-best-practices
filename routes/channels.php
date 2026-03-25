d68731499c6e007937eca350cba39adb65d9b7e1
Reda Ali <reda5201028@gmail.com>
2026-03-25 21:31:09 +0200

    Broadcasting

---
+++ routes/channels.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Broadcast;
+
+Broadcast::channel('App.Models.User.{id}', fn($user, $id) => (int) $user->id === (int) $id);
+
+Broadcast::private('admin.batch.{batchId}', fn($user, $batchId) => $user->isAdmin());
